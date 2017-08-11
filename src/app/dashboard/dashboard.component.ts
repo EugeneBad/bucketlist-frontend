d200f541fa233def17d7a52d30b9f39f1db9521e
eugenius <eugene.badriku@andela.com>
2017-08-11 13:21:42 +0300

    Add condition for displaying next and prev buttons

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,6 +8,7 @@ import { Http, Headers } from '@angular/http';
 })
 export class DashboardComponent implements OnInit {
   token: string;
+  response:any;
   bucketlists: any;
   offset: number = 1;
   q: string = '';
@@ -28,7 +29,9 @@ export class DashboardComponent implements OnInit {
     headers.append('token', this.token);
 
     this.http.get(`http://localhost:5000/api/V1/bucketlists?limit=4&offset=${this.offset}&q=${this.q}`, { headers: headers })
-      .subscribe(data => { this.bucketlists = JSON.parse(JSON.parse(JSON.stringify(data))._body).Bucketlists })
+      .subscribe(data => { this.response = JSON.parse(JSON.parse(JSON.stringify(data))._body);
+                           this.bucketlists = this.response.Bucketlists
+                         });
 
   }
 
