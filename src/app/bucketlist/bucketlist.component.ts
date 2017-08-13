f08e2bcec6281928f5c2ca4fb656cf7a58dd15e4
eugenius <eugene.badriku@andela.com>
2017-08-13 11:37:26 +0300

    Move bucketlist html and css from dashboard component to bucketlist component

--- src/app/bucketlist/bucketlist.component.ts
+++ src/app/bucketlist/bucketlist.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-bucketlist',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BucketlistComponent implements OnInit {
 
+  @Input() bucketlist;
+  
   constructor() { }
 
   ngOnInit() {
