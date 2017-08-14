ab8a6832ed9d604e82efcac6a5f35d5e20fb4239
eugenius <eugene.badriku@andela.com>
2017-08-14 17:57:46 +0300

    Add constructor to logged-in.guard

--- src/app/logged-in.guard.ts
+++ src/app/logged-in.guard.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { GetBucketlistsService } from './get-bucketlists.service';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class LoggedInGuard implements CanActivate {
-  canActivate(){
-    console.log('Works');
+  constructor(private authService: GetBucketlistsService, private router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     return true;
   }
 }
