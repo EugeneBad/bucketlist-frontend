213f768ef3d25fb1078a7919e8b0de671db368ec
eugenius <eugene.badriku@andela.com>
2017-08-16 19:56:59 +0300

    Remove edit/delete button animation

--- src/app/bucketlist/bucketlist.component.ts
+++ src/app/bucketlist/bucketlist.component.ts
@@ -12,18 +12,6 @@ export class BucketlistComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.refresh_jq();
-  }
-
-  refresh_jq(){
-    let script = document.getElementById('bcktlst_btns');
-    let head = document.getElementsByTagName('head')[0];
-    script.parentNode.removeChild(script);
-
-    let rescript = document.createElement('script');
-    rescript.src = 'assets/js/bcktlst_btns.js';
-    rescript.id = 'bcktlst_btns';
-    head.appendChild(rescript);
   }
 
 }
--- src/assets/js/bcktlst_btns.js
+++ src/assets/js/bcktlst_btns.js
@@ -4,11 +4,4 @@ $(document).ready(function() {
     $('#new_bcktlst_div').slideToggle();
   });
 
-  $('.item_btn').click(function(data) {
-    console.log(data.type);
-  });
-
-  $('.bucketlist').hover(function(e){console.log(e.type, e.currentTarget.id)},
-  function(e){console.log(e.currentTarget.id)});
-
 });
