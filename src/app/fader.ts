5bbbdddcf3badeeced605db99ac8cadc5d86ca59
eugenius <eugene.badriku@andela.com>
2017-08-23 17:47:51 +0300

    Set back button to appear with items

--- src/app/fader.ts
+++ src/app/fader.ts
@@ -3,6 +3,6 @@ import { trigger, state, style, animate, transition } from '@angular/core';
 export let faderAnimation = trigger('fader', [
                               state('in', style({ opacity: 1 })),
                               state('out', style({ opacity: 0 })),
-                              transition('* => in', animate('1s 0.9s ease-out')),
-                              transition('* => out', animate('1s 0.9s ease-out'))
+                              transition('* => in', animate('1s 0.7s ease-out')),
+                              transition('* => out', animate('1s 0.7s ease-out'))
                             ]);
